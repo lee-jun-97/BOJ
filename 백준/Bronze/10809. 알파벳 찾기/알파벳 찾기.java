c3b5ed86cfec568c2c365f0e045899e4b7de888e
Lee Jun <yuog12@naver.com>
2023-03-04 21:52:23 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 148 ms, Memory: 16312 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String input = br.readLine();
+        
+        System.out.print(input.indexOf('a') + " ");
+        System.out.print(input.indexOf('b') + " ");
+        System.out.print(input.indexOf('c') + " ");
+        System.out.print(input.indexOf('d') + " ");
+        System.out.print(input.indexOf('e') + " ");
+        System.out.print(input.indexOf('f') + " ");
+        System.out.print(input.indexOf('g') + " ");
+        System.out.print(input.indexOf('h') + " ");
+        System.out.print(input.indexOf('i') + " ");
+        System.out.print(input.indexOf('j') + " ");
+        System.out.print(input.indexOf('k') + " ");
+        System.out.print(input.indexOf('l') + " ");
+        System.out.print(input.indexOf('m') + " ");
+        System.out.print(input.indexOf('n') + " ");
+        System.out.print(input.indexOf('o') + " ");
+        System.out.print(input.indexOf('p') + " ");
+        System.out.print(input.indexOf('q') + " ");
+        System.out.print(input.indexOf('r') + " ");
+        System.out.print(input.indexOf('s') + " ");
+        System.out.print(input.indexOf('t') + " ");
+        System.out.print(input.indexOf('u') + " ");
+        System.out.print(input.indexOf('v') + " ");
+        System.out.print(input.indexOf('w') + " ");
+        System.out.print(input.indexOf('x') + " ");
+        System.out.print(input.indexOf('y') + " ");
+        System.out.print(input.indexOf('z'));
+    }
+}

21a03e0bb0ca5109462583213acd0cf30e77d6bb
Lee Jun <yuog12@naver.com>
2023-03-04 22:14:23 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] input = br.readLine().split(" ");
+        System.out.println(Integer.parseInt(input[0]) * Integer.parseInt(input[1]));
+    }
+}

0c5a90a7f24e121dd006aa994dbc6618c5da2bc4
Lee Jun <yuog12@naver.com>
2023-03-16 22:07:26 +0900

    [unrated] Title: 카드 뭉치, Time: 0.15 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+        
+        for(String i : cards1) {
+            queue1.add(i);
+        }
+        
+        for(String i : cards2) {
+            queue2.add(i);
+        }
+        
+        String answer = "Yes";
+        
+        for(String i : goal) {
+            if(!queue1.isEmpty() && queue1.peek().equals(i)) {
+                queue1.poll();
+            } else if(!queue2.isEmpty() && queue2.peek().equals(i)) {
+                queue2.poll();
+            } else {
+                answer = "No";
+            }
+        }
+        
+        return answer;
+    }
+}

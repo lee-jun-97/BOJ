ef369b8691e711d997708e4cd25a62e2eb973e3a
Lee Jun <yuog12@naver.com>
2023-04-12 10:59:46 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -12,9 +12,11 @@ public class Main {
         String str = br.readLine();
 
         BigDecimal sum = new BigDecimal(0);
+        int r = 1;
 
         for(int i=0; i<n; i++) {
-            sum = sum.add(new BigDecimal((str.charAt(i) - 'a' + 1) * Math.pow(31, i)));
+            sum = sum.add(new BigDecimal((str.charAt(i) - 'a' + 1) * r % 1234567891));
+            r = (r*31) % 1234567891;
         }
 
         System.out.println(sum);

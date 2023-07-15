b5587be03b04574242daaa418ced7eafec63b690
Lee Jun <yuog12@naver.com>
2023-07-15 21:13:46 +0900

    [level 3] Title: 단어 변환, Time: 0.55 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    
+    public class Node {
+        String s;
+        int count;
+        boolean[] visited;
+        
+        public Node(String s, int count, boolean[] visited) {
+            this.s = s;
+            this.count = count;
+            this.visited = visited;
+        }
+    }
+    
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        
+        boolean[] visited = new boolean[words.length];
+        Queue<Node> q = new LinkedList<>();
+        
+        q.add(new Node(begin, 0, visited));
+        
+        while(!q.isEmpty()) {
+            
+            Node node = q.poll();
+            
+            if(node.s.equals(target)) {
+                answer = node.count;
+                break;
+            }
+            
+            char[] arr = node.s.toCharArray();
+            
+            for(int i=0; i<begin.length(); i++) {
+                char temp = arr[i];
+                arr[i] = '.';
+                
+                for(int j=0; j<words.length; j++) {
+                    String s = String.valueOf(arr);
+
+                    if(!node.visited[j] && words[j].matches(s)) {
+                        node.visited[j] = true;
+                        q.add(new Node(words[j], node.count + 1, node.visited));
+                    }
+                }
+                
+                arr[i] = temp;
+            }
+        }
+        
+        return answer;
+    }
+}
